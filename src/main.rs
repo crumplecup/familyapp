14a5e2bcb2a8adeeea7ab07cf65c7250b55666af
crumplecup <erik.w.rose@gmail.com>
2023-09-06 11:19:49 -0700

    melted

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use anyhow::anyhow;
 use axum::{routing::get, Router};
+use familyapp::message;
 use shuttle_runtime::CustomError;
-use sqlx::{Executor, PgPool};
 use shuttle_secrets::SecretStore;
+use sqlx::{Executor, PgPool};
 use std::path::PathBuf;
 use tower_http::services::ServeDir;
+use tracing::info;
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
@@ -14,7 +16,9 @@ async fn hello_world() -> &'static str {
 async fn axum(
     #[shuttle_secrets::Secrets] secret_store: SecretStore,
     #[shuttle_static_folder::StaticFolder] static_folder: PathBuf,
-    #[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_axum::ShuttleAxum {
+    // #[shuttle_shared_db::Postgres] pool: PgPool) -> Result<shuttle_axum::AxumService, error::UiError> {
+    #[shuttle_shared_db::Postgres] pool: PgPool,
+) -> shuttle_axum::ShuttleAxum {
     let secret = if let Some(pass) = secret_store.get("ADMIN_PASSWORD") {
         pass
     } else {
@@ -23,8 +27,10 @@ async fn axum(
     pool.execute(include_str!("../schema.sql"))
         .await
         .map_err(CustomError::new)?;
-    let router = Router::new().route("/", get(hello_world))
+    let router = Router::new()
+        .route("/", get(hello_world))
         .nest_service("/assets", ServeDir::new(static_folder));
 
+    info!("🚀 Server started successfully");
     Ok(router.into())
 }

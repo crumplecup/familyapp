ce6003cec28233505d24b827a5f017455cd5216a
crumplecup <erik.w.rose@gmail.com>
2023-11-18 11:00:47 -0800

    prepare to delete

--- src/main.rs
+++ src/main.rs
@@ -13,22 +13,22 @@ async fn hello_world() -> &'static str {
 
 #[shuttle_runtime::main]
 async fn axum(
-    #[shuttle_secrets::Secrets] secret_store: SecretStore,
-    #[shuttle_static_folder::StaticFolder] static_folder: PathBuf,
+    // #[shuttle_secrets::Secrets] secret_store: SecretStore,
+    // #[shuttle_static_folder::StaticFolder] static_folder: PathBuf,
     // #[shuttle_shared_db::Postgres] pool: PgPool) -> Result<shuttle_axum::AxumService, error::UiError> {
-    #[shuttle_shared_db::Postgres] pool: PgPool,
+    // #[shuttle_shared_db::Postgres] pool: PgPool,
 ) -> shuttle_axum::ShuttleAxum {
-    let secret = if let Some(pass) = secret_store.get("ADMIN_PASSWORD") {
-        pass
-    } else {
-        return Err(anyhow!("Password was not found").into());
-    };
-    pool.execute(include_str!("../schema.sql"))
-        .await
-        .map_err(CustomError::new)?;
+    // let secret = if let Some(pass) = secret_store.get("ADMIN_PASSWORD") {
+    //     pass
+    // } else {
+    //     return Err(anyhow!("Password was not found").into());
+    // };
+    // pool.execute(include_str!("../schema.sql"))
+    //     .await
+    //     .map_err(CustomError::new)?;
     let router = Router::new()
-        .route("/", get(hello_world))
-        .nest_service("/assets", ServeDir::new(static_folder));
+        .route("/", get(hello_world));
+        // .nest_service("/assets", ServeDir::new(static_folder));
 
     info!("🚀 Server started successfully");
     Ok(router.into())

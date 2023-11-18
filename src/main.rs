30138ad19466e8add557ba632ed101b043e64f2d
crumplecup <erik.w.rose@gmail.com>
2023-11-18 11:10:23 -0800

    prepare to delete

--- src/main.rs
+++ src/main.rs
@@ -16,16 +16,16 @@ async fn axum(
     // #[shuttle_secrets::Secrets] secret_store: SecretStore,
     // #[shuttle_static_folder::StaticFolder] static_folder: PathBuf,
     // #[shuttle_shared_db::Postgres] pool: PgPool) -> Result<shuttle_axum::AxumService, error::UiError> {
-    // #[shuttle_shared_db::Postgres] pool: PgPool,
+    #[shuttle_shared_db::Postgres] pool: PgPool,
 ) -> shuttle_axum::ShuttleAxum {
     // let secret = if let Some(pass) = secret_store.get("ADMIN_PASSWORD") {
     //     pass
     // } else {
     //     return Err(anyhow!("Password was not found").into());
     // };
-    // pool.execute(include_str!("../schema.sql"))
-    //     .await
-    //     .map_err(CustomError::new)?;
+    pool.execute(include_str!("../schema.sql"))
+        .await
+        .map_err(CustomError::new)?;
     let router = Router::new()
         .route("/", get(hello_world));
         // .nest_service("/assets", ServeDir::new(static_folder));
